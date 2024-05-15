d9c2fe71e62148b9141db3d00605d79213757e28
theskinnycoder <18131a0505@gvpce.ac.in>
2024-05-15 14:11:59 +0530

    fix types

--- packages/@calender-sync/react/src/components/dropdown/menu.tsx
+++ packages/@calender-sync/react/src/components/dropdown/menu.tsx
@@ -1,14 +1,30 @@
 import { DropdownMenu } from "@/components/ui/dropdown-menu"
 import * as React from "react"
+import {
+  CalendarSyncDropdownContext,
+  type CalendarSyncDropdownProps,
+} from "./context"
 
 interface CalSyncDropdownMenuProps
-  extends React.ComponentPropsWithoutRef<typeof DropdownMenu> {}
+  extends React.ComponentPropsWithoutRef<typeof DropdownMenu>,
+    CalendarSyncDropdownProps {}
 
 const CalSyncDropdownMenu = ({
   children,
+  event,
+  newTab = true,
   ...rest
 }: CalSyncDropdownMenuProps) => {
-  return <DropdownMenu {...rest}>{children}</DropdownMenu>
+  return (
+    <CalendarSyncDropdownContext.Provider
+      value={{
+        event,
+        newTab,
+      }}
+    >
+      <DropdownMenu {...rest}>{children}</DropdownMenu>
+    </CalendarSyncDropdownContext.Provider>
+  )
 }
 
 CalSyncDropdownMenu.displayName = "CalSyncDropdown.Menu"
